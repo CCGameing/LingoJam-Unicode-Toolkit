07f141c38aa0b4c84829071cb6ff43f8f9d64539
Clayton Cornell <81330047+CCGameing@users.noreply.github.com>
2024-06-03 13:48:36 -0400

    6/3/2024 - 1:48 gui container test

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-alert("Import Successful!")
+console.log("Repo Import Successful!")
+
+//#region GUI Container
+
+var guiContainer = $("<div></div>")
+guiContainer.addClass("guiContainer")
+
+$("#english-text").parent().append(container)
+$(".main-title").children()[0].innerHTML = 'ᵘη𝓲𝒸ᗝⒹԐ: Ⓣ𝕠𝓞𝓵ᛕ𝐢Ｔ'
+
+//#endregion
